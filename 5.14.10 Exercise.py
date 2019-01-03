64ec69adc96f73b5f1f57b562c681de310fd7313
Duc Le <duyduc100@gmail.com>
2019-01-03 12:48:37 +0700

    Delete 5.14.10 Exercise.py

--- 5.14.10 Exercise.py
+++
@@ -1,9 +0,0 @@
-# Exercise 5.14.10 OK
-def find_hypot():
-    a = int(input("Enter a length of a side: "))
-    b = int(input("Enter a length of a side: "))
-    c = (a**2 + b**2)**0.5
-
-    print("Length of the hypotenuse of this triangle is:", c)
-
-find_hypot()
