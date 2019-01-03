25c5ea86fb9a2ec28635bdbba15beacfb751b1f4
Duc Le <duyduc100@gmail.com>
2019-01-03 12:36:00 +0700

    Add files via upload

---
+++ Chap 9/9.6. (from 1 to 3) Exercise.py
@@ -0,0 +1,14 @@
+# Exercise 9.6.1 2 3
+
+my_tup = ("Name", "Age", "Location")
+
+def test_tuple(tup):
+    print ("We have that types: ", tup)
+
+test_tuple(my_tup)
+
+# Tuple can use as a argument of python function
+
+# A pair is a generalization of a tuple
+
+# A pair is a kind of tuple. Because each element of pair not type of tuple
